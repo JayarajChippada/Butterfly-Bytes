de4fc4647d014a52ed109029d4034a220a631b83
JayarajChippada <jayarajchippada4@gmail.com>
2024-07-10 21:31:31 +0530

    React and TailwindCss setup is done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 flex items-center justify-center p-5">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
