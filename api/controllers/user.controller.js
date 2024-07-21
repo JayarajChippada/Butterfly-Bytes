6255b125bb18c94bbd0e204f9af15479dde02513
JayarajChippada <jayarajchippada4@gmail.com>
2024-07-21 15:56:28 +0530

    Created get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -65,3 +65,39 @@ export const signout = (req, res, next) => {
         next(error);
     }
 }
+
+export const getUsers = async(req, res, next) => {
+    if(!req.user.isAdmin) {
+        return next(errorHandler(403, "You are not allowed to see all users"))
+    }
+    try{
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === 'asc' ? 1 : -1; // newest and oldest
+        const users = await User.find()
+                        .sort({ updatedAt: sortDirection })
+                        .skip(startIndex)
+                        .limit(limit);
+        
+        const usersWithoutPasswords = users.map((user) => {
+            const { password: pass, ...userData } = user._doc;
+            return userData;
+        })
+        const totalUsers = await User.countDocuments();
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        ); 
+        const lastMonthUsers = await User.countDocuments({ createdAt: { $gte: oneMonthAgo }}) //gte-greater than equal to 
+
+        res.status(200).json({
+            usersWithoutPasswords,
+            totalUsers,
+            lastMonthUsers
+        })
+    } catch(error) {
+        next(error);
+    }
+}
